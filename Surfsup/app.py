99ad2dd56f9c6705c6f8850edb726ec3db7db12c
Dan Kramer <dan.r.kramer@gmail.com>
2024-03-05 16:04:29 -0800

    Updated files after tutoring assistance. Project complete!

--- Surfsup/app.py
+++ Surfsup/app.py
@@ -2,6 +2,7 @@
 import numpy as np
 
 import sqlalchemy
+import datetime as dt
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
@@ -102,33 +103,41 @@ def temperatures():
 
 @app.route("/api/v1.0/<start>")
 def start_temps(start):
-    
+    temp_data = "Date not found."
+
     results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
     filter(Measurement.date >= start).all()
 
+    if results[0][0] is not None:
+
+        temp_data = list(np.ravel(results))
+    
+
     session.close()
+    return jsonify(temp_data)
 
-    temp_data = list(np.ravel(results))
 
-    return (temp_data)
-
-    #for date in results:
-        #if start == date:
-            #return jsonify(temp_data)
-        
-    #else: return jsonify({"error": "Date not found."}), 404
     
 @app.route("/api/v1.0/<start>/<end>")
 def range_temps(start, end):
+    temp_data = "This range of dates is not covered in the data!"
 
+## Help from tutor Justin to set up validation message for user - did not finish, but confirms dates work!
+    start_date = dt.date(*[int(x) for x in start.split("-")])
+    end_date = dt.date(*[int(x) for x in end.split("-")])
+
+    if start_date < end_date:
+        print("valid range")
+    
+    
     results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
     filter(Measurement.date >= start).\
     filter(Measurement.date <= end).all()
 
-    session.close()
 
     temp_data = list(np.ravel(results))
 
+    session.close()
     return (temp_data)
 
 
